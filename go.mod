1db363b268689aee7f5dd867b3556292a2f607c7
podhmo <ababjam61+github@gmail.com>
2023-01-29 20:20:10 +0900

    Catchup (#9)
    * fix: support pagination
    
    * chore: update go.mod
    
    * fix: update main.go
    
    * chore: update openapi.json
    
    * feat: use go:embed

--- go.mod
+++ go.mod
@@ -3,40 +3,46 @@ module github.com/podhmo/gtasks-server
 go 1.18
 
 require (
-	github.com/go-chi/chi/v5 v5.0.7
-	github.com/go-chi/render v1.0.1
-	github.com/gomarkdown/markdown v0.0.0-20220905174103-7b278df48cfb
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/render v1.0.2
+	github.com/gomarkdown/markdown v0.0.0-20221013030248-663e2500819c
 	github.com/google/uuid v1.3.0
 	github.com/podhmo/flagstruct v0.5.0
-	github.com/podhmo/quickapi v0.0.0-20220720175550-0b7945ed5305
-	github.com/podhmo/reflect-openapi v0.1.1
-	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c
-	google.golang.org/api v0.84.0
+	github.com/podhmo/quickapi v0.0.0
+	github.com/podhmo/reflect-openapi v0.2.0
+	golang.org/x/oauth2 v0.4.0
+	google.golang.org/api v0.108.0
 )
 
 require (
-	cloud.google.com/go/compute v1.7.0 // indirect
-	github.com/getkin/kin-openapi v0.97.0 // indirect
-	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/swag v0.21.1 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	cloud.google.com/go/compute v1.18.0 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/getkin/kin-openapi v0.113.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.0.0-20220520183353-fd19c99a87aa // indirect
-	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.1 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/podhmo/reflect-shape v0.3.5 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/perimeterx/marshmallow v1.1.4 // indirect
+	github.com/podhmo/commentof v0.1.3 // indirect
+	github.com/podhmo/reflect-shape v0.4.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
-	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
-	golang.org/x/text v0.3.7 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220616135557-88e70c0c3a90 // indirect
-	google.golang.org/grpc v1.47.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/genproto v0.0.0-20230127162408-596548ed4efa // indirect
+	google.golang.org/grpc v1.52.3 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
+//go:generate go run ./ --gendoc --docfile openapi.json
 package main
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -17,23 +20,27 @@ import (
 	"github.com/podhmo/flagstruct"
 	"github.com/podhmo/gtasks-server/auth"
 	"github.com/podhmo/quickapi"
-	"github.com/podhmo/quickapi/experimental/define"
-	rohandler "github.com/podhmo/reflect-openapi/handler"
+	"github.com/podhmo/quickapi/qopenapi/define"
+	"github.com/podhmo/reflect-openapi/dochandler"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/tasks/v1"
 )
 
-type Config struct {
+//go:embed openapi.json
+var openapiDocData []byte
+
+type Options struct {
 	ClientID     string `flag:"client-id" required:"true"`
 	ClientSecret string `flag:"client-secret" required:"true"`
 	RedirectURL  string `flag:"redirect-url" required:"true"`
 
-	GenDoc bool `flag:"gendoc" help:"generate openapi.json to stdout"`
+	GenDoc  bool   `flag:"gendoc" help:"generate openapi.json to stdout"`
+	Docfile string `flag:"docfile" help:"write name of openapi.json"`
 }
 
 func main() {
-	config := Config{RedirectURL: "http://localhost:8888"}
+	config := Options{RedirectURL: "http://localhost:8888"}
 	flagstruct.Parse(&config)
 	if err := run(config); err != nil {
 		log.Printf("!! %+v", err)
@@ -44,13 +51,13 @@ var SCOPES = []string{
 	"https://www.googleapis.com/auth/tasks",
 }
 
-func run(config Config) error {
+func run(options Options) error {
 	// Your credentials should be obtained from the Google
 	// Developer Console (https://console.developers.google.com).
 	conf := &oauth2.Config{
-		ClientID:     config.ClientID,
-		ClientSecret: config.ClientSecret,
-		RedirectURL:  config.RedirectURL + "/auth/callback",
+		ClientID:     options.ClientID,
+		ClientSecret: options.ClientSecret,
+		RedirectURL:  options.RedirectURL + "/auth/callback",
 		Scopes:       SCOPES,
 		Endpoint:     google.Endpoint,
 	}
@@ -70,7 +77,14 @@ func run(config Config) error {
 	router.Get("/auth/login", auth.Login)
 	router.Get("/auth/callback", auth.Callback)
 
-	doc := define.Doc().Server(strings.TrimSuffix(auth.DefaultURL, "/api/tasklist"), "local development").Title("gtask-server")
+	doc := define.Doc().
+		Server(strings.TrimSuffix(auth.DefaultURL, "/api/tasklist"), "local development").
+		Title("gtask-server")
+
+	if !options.GenDoc {
+		doc = doc.LoadFromData(openapiDocData)
+	}
+
 	bc, err := define.NewBuildContext(doc, router)
 	if err != nil {
 		return fmt.Errorf("build context: %w", err)
@@ -83,7 +97,7 @@ func run(config Config) error {
 			api := &TaskListAPI{Oauth2Config: auth.OauthConfig}
 			define.Get(bc, path, api.List,
 				auth.WithOauthToken(":default-key:"),
-			).OperationID("ListTaskList")
+			)
 		}
 		{
 			api := &TaskAPI{Oauth2Config: auth.OauthConfig}
@@ -92,7 +106,7 @@ func run(config Config) error {
 
 				define.Get(bc, path, api.List,
 					auth.WithOauthToken(":default-key:"),
-				).OperationID("ListTasksOfTaskList")
+				)
 			}
 		}
 		{
@@ -101,13 +115,13 @@ func run(config Config) error {
 				path := "/"
 				define.GetHTML(bc, path, api.ListTaskList, dumpMarkdown,
 					auth.WithOauthToken(":default-key:"),
-				).OperationID("index")
+				)
 			}
 			{
 				path := "/{tasklistId}"
 				define.GetHTML(bc, path, api.DetailTaskList, dumpMarkdown,
 					auth.WithOauthToken(":default-key:"),
-				).OperationID("detail")
+				)
 			}
 
 		}
@@ -115,11 +129,23 @@ func run(config Config) error {
 
 	// mount optional handler (not included in openapi.json)
 	{
-		bc.Router().Mount("/openapi", rohandler.NewHandler(bc.Doc(), "/openapi"))
+		bc.Router().Mount("/openapi", dochandler.New(bc.Doc(), "/openapi"))
 	}
 
-	if config.GenDoc {
-		return bc.EmitDoc(ctx, os.Stdout)
+	if options.GenDoc {
+		var w io.Writer = os.Stdout
+		if options.Docfile != "" {
+			f, err := os.Create(options.Docfile)
+			if err != nil {
+				return fmt.Errorf("write file: %w", err)
+			}
+			defer f.Close()
+			w = f
+		}
+		if err := bc.EmitDoc(ctx, w); err != nil {
+			return err
+		}
+		return nil
 	}
 
 	h, err := bc.BuildHandler(ctx)
@@ -127,9 +153,9 @@ func run(config Config) error {
 		return fmt.Errorf("build handler: %w", err)
 	}
 
-	u, err := url.Parse(config.RedirectURL)
+	u, err := url.Parse(options.RedirectURL)
 	if err != nil {
-		return fmt.Errorf("invalid url: %q -- %w", config.RedirectURL, err)
+		return fmt.Errorf("invalid url: %q -- %w", options.RedirectURL, err)
 	}
 	srv := quickapi.NewServer(fmt.Sprintf(":%s", u.Port()), h, 5*time.Second)
 	return srv.ListenAndServe(ctx)
@@ -164,7 +190,7 @@ type TaskAPI struct {
 }
 
 type TaskAPIListInput struct {
-	TaskListID string `openapi:"path" path:"tasklistId"`
+	TaskListID string `in:"path" path:"tasklistId"`
 }
 
 func (api *TaskAPI) List(ctx context.Context, input TaskAPIListInput) ([]*tasks.Task, error) {
@@ -241,7 +267,7 @@ func (api *MarkdownAPI) ListTaskList(ctx context.Context, input quickapi.Empty) 
 }
 
 type MarkdownAPIDetailTaskListInput struct {
-	TaskListID string `openapi:"path" path:"tasklistId"`
+	TaskListID string `in:"path" path:"tasklistId"`
 }
 
 func (api *MarkdownAPI) DetailTaskList(ctx context.Context, input MarkdownAPIDetailTaskListInput) (string, error) {
